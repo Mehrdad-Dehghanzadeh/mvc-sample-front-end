b33948cd3c036b6a8a8d8e3e139c4b560c6b4c99
Mehrdad Dehghanzadeh <mehrdad.dehghanzadeh.dev@gmail.com>
2021-03-27 23:41:34 +0430

    build: config purge css

--- postcss.config.js
+++ postcss.config.js
@@ -1,5 +1,20 @@
+const purgecss = require('@fullhuman/postcss-purgecss')
+
 module.exports = {
-  plugins: [
-    require('autoprefixer')
-  ]
+    plugins: [
+        require('autoprefixer'),
+
+        purgecss({
+            safelist: {
+                standard: [
+                    /--active$/
+                ],
+                deep: [],
+                greedy: [],
+                keyframes: [],
+                variables: []
+            },
+            content: ['./src/views/*.html']
+        }),
+    ]
 }
