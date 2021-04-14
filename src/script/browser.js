0d9323cb5bb9ba0b74878cfbe3b134692709dbf0
Mehrdad Dehghanzadeh <mehrdad.dehghanzadeh.dev@gmail.com>
2021-04-14 23:53:46 +0430

    feat: add not support browser

--- src/script/browser.js
+++ src/script/browser.js
@@ -1,3 +1,7 @@
+/** 
+ * @return { Object }  browser names and Version
+ * browser: Firefox | Chrome | Opera | IE | Safari
+ */
 navigator.saysWho = (function () {
     const {
         userAgent
@@ -8,20 +12,29 @@ navigator.saysWho = (function () {
     if (/trident/i.test(match[1])) {
         temp = /\brv[ :]+(\d+)/g.exec(userAgent) || []
 
-        return `IE ${temp[1] || ''}`
+        return {
+            browser: 'IE ',
+            version: temp[1] || ''
+        }
     }
 
     if (match[1] === 'Chrome') {
         temp = userAgent.match(/\b(OPR|Edge)\/(\d+)/)
 
         if (temp !== null) {
-            return temp.slice(1).join(' ').replace('OPR', 'Opera')
+            return {
+                browser: temp[1].replace('OPR', 'Opera'),
+                version: temp[0].slice(4)
+            }
         }
 
         temp = userAgent.match(/\b(Edg)\/(\d+)/)
 
         if (temp !== null) {
-            return temp.slice(1).join(' ').replace('Edg', 'Edge (Chromium)')
+            return {
+                browser: temp[1].replace('Edg', 'Edge'),
+                version: temp[0].slice(4)
+            }
         }
     }
     match = match[2] ? [match[1], match[2]] : [navigator.appName, navigator.appVersion, '-?']
@@ -33,7 +46,25 @@ navigator.saysWho = (function () {
 
 
     return {
-        browser: match[0],
+        browser: match[0] === "MSIE" ? "IE" : match[0],
         version: Number(match[1])
     }
 })()
+
+function checkBrowser(url) {
+    var defualt = {
+        IE: 11,
+        Safari: 6.1,
+        Firefox: 28,
+        Chrome: 28,
+        Opera: 12.1
+    };
+
+    var keys = Object.keys(defualt);
+
+    if (keys.indexOf(navigator.saysWho.browser) != -1 && defualt[navigator.saysWho.browser] > navigator.saysWho.version) {
+        location.replace(url);
+    }
+}
+
+checkBrowser("not-support.html")
